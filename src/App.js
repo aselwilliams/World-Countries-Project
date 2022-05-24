5b0d8acf3810d91be9ecf6271da9c68fdefba7a5
aselwilliams <aselwilliams22@gmail.com>
2022-05-24 11:39:31 -0400

    changed to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,122 +1,130 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
-import {Component} from 'react';
-import Pagination from 'react-bootstrap/Pagination';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Pagination from './components/Pagination';
-// import Card from './components/Card';
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage,setItemsPerPage] = useState(18)
 
-class App extends Component {
-constructor(){
-  super()
-  this.state = {
-    countries: [],
-    isLoading: false,
-    index: 0,
-    currentPage: 1,
-    // pageNumber: index+1
-
-  }
-}
-componentDidMount(){
-  const url = "https://restcountries.com/v3.1/all";
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) =>
+  const fetchData = (url) => {
     setTimeout(() => {
-      this.setState({ countries: data, isLoading: true });
-    }, 1000)
-  )
-  .catch((error)=>console.log(error))
-}
+      fetch("https://restcountries.com/v3.1/all")
+        .then((res) => res.json())
+        .then((data) => setCountries(data))
+        .catch((err) => console.log(err));
+    }, 1000);
+  };
 
-handlePrev=()=>{
-  console.log('clicked prev')
-  const {index, countries} = this.state;
-  if(index>0){
-this.setState({index: index - 1})
-  }
-}
+  useEffect(() => {
+    // setIsLoading(true);
+    fetchData();
+    setIsLoading(false);
+  }, [currentPage]);
 
-handleNext=()=>{
-  console.log('next is clicked')
-  const pagesCount=Math.floor(this.state.countries.length/21);
-  const {index,countries} = this.state;
-  if(index < pagesCount) {
-    this.setState({index: index + 1})
-  }
-}
+  //Get current countries
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = countries.slice(indexOfFirstItem, indexOfLastItem);
 
-handleFirst=()=> {
-this.setState({index: 0})
-}
+  const changePage = (page) => {
+    console.log(page);
+    setCurrentPage(page);
+  };
 
-handleLast=()=> {
-  const pagesCount = Math.floor(this.state.countries.length/21);
-  this.setState({index: pagesCount})
-}
-
-changePage=(num)=> {
-  this.setState({index:num})
-}
-
-render() {
-  // const pagesCount=Math.ceil(countries.length/21);
-  const {countries, isLoading, index} = this.state;
   return (
     <>
-    <div className='main-container'>
-      <h2 className='header'>WORLD COUNTRIES WITH REST API</h2>
-      <div className='pagination'>
-        <section>
-         <p className='count'>{countries.length} countries</p>
-        </section>
-        <section className='flex'>
-          <div>{index+1} / {Math.ceil(countries.length/21)}pages</div>
-          <div>
-          <Pagination>
-            <Pagination.First onClick={()=>this.handleFirst(index)} />
-            <Pagination.Prev onClick={this.handlePrev} />
-            <Pagination.Item onClick={()=>this.changePage(0)}>{1}</Pagination.Item>
-         
-            <Pagination.Item onClick={()=>this.changePage(1)}>{2}</Pagination.Item>
-            <Pagination.Item onClick={()=>this.changePage(2)}>{3}</Pagination.Item>
-            <Pagination.Item onClick={()=>this.changePage(3)}>{4}</Pagination.Item>
-            <Pagination.Item onClick={()=>this.changePage(4)}>{5}</Pagination.Item>
-          
-            <Pagination.Next onClick={this.handleNext} />
-            <Pagination.Last onClick={this.handleLast} />
-          </Pagination>
-          </div>
-        </section>
+      <div className="main-container">
+        <h2 className="header">WORLD COUNTRIES WITH REST API</h2>
+        <div className="pagination">
+          <section>
+            <p className="count">{countries.length} countries</p>
+          </section>
+          <section className="flex">
+            <div>
+              {currentPage} / {Math.ceil(countries.length / itemsPerPage)}pages
+            </div>
+            <div>
+              <Pagination>
+                <Pagination.First onClick={() => changePage(1)} />
+                <Pagination.Prev onClick={() => changePage(currentPage>1 ? currentPage - 1 : 1)} />
+                <Pagination.Item
+                  onClick={() =>
+                    changePage(currentPage > 5 ? currentPage - 4 : 1)
+                  }
+                >
+                  {currentPage > 5 ? currentPage - 4 : 1}
+                </Pagination.Item>
+
+                <Pagination.Item
+                  onClick={() =>
+                    changePage(currentPage > 5 ? currentPage - 3 : 2)
+                  }
+                >
+                  {currentPage > 5 ? currentPage - 3 : 2}
+                </Pagination.Item>
+                <Pagination.Item
+                  onClick={() =>
+                    changePage(currentPage > 5 ? currentPage - 2 : 3)
+                  }
+                >
+                  {currentPage > 5 ? currentPage - 2 : 3}
+                </Pagination.Item>
+                <Pagination.Item
+                  onClick={() =>
+                    changePage(currentPage > 5 ? currentPage - 1 : 4)
+                  }
+                >
+                  {currentPage > 5 ? currentPage - 1 : 4}
+                </Pagination.Item>
+                <Pagination.Item
+                  onClick={() => changePage(currentPage > 5 ? currentPage : 5)}
+                >
+                  {currentPage > 5 ? currentPage : 5}
+                </Pagination.Item>
+
+                <Pagination.Next onClick={() => changePage(currentPage<Math.ceil(countries.length / itemsPerPage) ? currentPage + 1 : Math.ceil(countries.length / itemsPerPage))} />
+                <Pagination.Last onClick={() => changePage(Math.ceil(countries.length / itemsPerPage))} />
+              </Pagination>
+            </div>
+          </section>
         </div>
-        <div className='countries'>
-          {isLoading ? (<>
-            <div className='card'>
-              {countries.map((country, index)=>
-                <section className='each-card'>
-                <img src={countries[index].flags.png} alt={countries[index].name.common} flag />
-                <h4>{countries[index].name.common}</h4>
-                <p><strong>Cap:</strong><span>{countries[index].capital}</span></p>
-                <p><strong>Pop:</strong><span>{countries[index].population}</span></p>
-              </section>
-              // <Card key={ind} ind={ind} country={country} />
-              )}
+        <div className="countries">
           
-        </div>
-          </>) : (
+              <div className="card">
+                {currentItems.map((country, index) => (
+                  <section className="each-card" key={index}>
+                    <img
+                      src={country.flags.png}
+                      alt={country.name.common}
+                      flag
+                    />
+                    <h4>{country.name.common}</h4>
+                    <p>
+                      <strong>Cap:</strong>
+                      <span>{country.capital}</span>
+                    </p>
+                    <p>
+                      <strong>Pop:</strong>
+                      <span>{country.population}</span>
+                    </p>
+                  </section>
+                ))}
+              </div>
+          {isLoading && 
             <i
               className="fa fa-spinner fa-spin"
               style={{ fontSize: "48px" }}
             ></i>
-          )}
+          }
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
-}
-export default App
 
+export default App;
 
-
+//   const url = "https://restcountries.com/v3.1/all";
